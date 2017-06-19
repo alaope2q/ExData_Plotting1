d4a1bb782db8a717a70a02b13a5042bb9669bc3f
alaope2q <alaoptr@gmail.com>
2017-06-19 17:17:30 +0200

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -46,33 +46,3 @@ png("plot1.png", width=480, height=480)
 hist(Global.active.power, col="red", main="Global Active Power", xlab="Global Active Power (kW)")
 dev.off()
 
-
-######################## PLOT 2 ###################################
-# We have two columns for time, hence we should combine the two columns using the paste
-# function and a seperator, I chose to use space i.e " " 
-# the format also has to be defined in order to include the time up till seconds for this
-#exercise. We could do this in two steps by first combining the first two columns with
-# the following command 
-#Date.and.time <- (paste(subenergy$Date, subenergy$Time, sep=" ")
-#Date.and.time <- strptime(Date.and.time, "%d/%m/%Y %H:%M:%S") 
-#Date.and.time <- as.Date(energy$Date, "%d%m%Y")
-# I chose to do this with a single command
-
-Date.and.time <- strptime(paste(subenergy$Date, subenergy$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-
-# now we plot the chart and specify it should plot a line instead of the default circles
-plot(Date.and.time, Global.active.power, type = "l", xlab="Days", ylab="Global Active Power (kilowatts)")
-
-# just like the other tasks, we define the parameters for metering as numeric values
-sub.metering1 <- as.numeric(subenergy$Sub_metering_1)
-sub.metering2 <- as.numeric(subenergy$Sub_metering_2)
-sub.metering3 <- as.numeric(subenergy$Sub_metering_3)
-
-png("plot3.png", width=480, height=480)
-plot(Date.and.time, sub.metering1, type="l", ylab="Energy sub metering", xlab="Weekdays")
-lines(Date.and.time, sub.metering2, type="l", col="red")
-lines(Date.and.time, sub.metering3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"))
-dev.off()
-
-
